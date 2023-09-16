e59937029ea8c1ca0717130092e4434f889042e4
heliosalesjr <heliosalesjr@gmail.com>
2023-09-16 12:09:42 -0300

    basic setup + 1st page

---
+++ app/page.tsx
@@ -0,0 +1,58 @@
+import Head from "next/head"
+import { BsFillMoonStarsFill } from 'react-icons/bs'
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube} from 'react-icons/ai'
+import heliogiraffe from "../public/heliogiraffe 1.png"
+import Image from "next/image"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | Portfolio</title>
+        <meta name="description" content="Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+        <section className=" min-h-screen">
+
+          <nav className=" py-10 mb-12 flex justify-between dark:text-white">
+            <h1 className="font-burtons text-xl font-extrabold">heliosalesjr</h1>
+          
+
+            <ul className="flex items-center">
+              <li><BsFillMoonStarsFill /></li>
+              <li>
+                <a className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8" href="#">Resume</a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="text-center p-10 py-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
+              Hélio Sales Jr.
+            </h2>
+            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+              Developer and game designer.
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+              I'm a developer and game designer based in Brazil, trying to build a better web byte by byte.  
+            </p>
+            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+              <AiFillTwitterCircle />
+              <AiFillLinkedin />
+              <AiFillYoutube />
+            </div>
+            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+              <Image src={heliogiraffe} alt="Helio and a Giraffe" layout="fill" objectFit="cover" />
+            </div>
+          </div>
+          
+        </section>
+
+      </main>
+      
+    
+    </>
+  )
+}
