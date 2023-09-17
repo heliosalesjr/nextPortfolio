e875f97503f40341dcb97c9864e0cf2bcfd2abbd
heliosalesjr <heliosalesjr@gmail.com>
2023-09-17 20:23:20 -0300

    added cards and bio

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import { BsFillMoonStarsFill } from 'react-icons/bs'
 import {AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube} from 'react-icons/ai'
 import heliogiraffe from "../public/heliogiraffe 1.png"
 import Image from "next/image"
+import design from "../public/design.png"
+import code from "../public/code.png"
+import consulting from "../public/consulting.png"
+//https://github.com/developedbyed/react-portofolio-with-tailwind/blob/main/pages/index.js
 
 export default function Home() {
   return (
@@ -48,6 +52,63 @@ export default function Home() {
             </div>
           </div>
           
+        </section>
+
+        <section>  {/* Second section*/}
+          <div>
+
+            <h3 className="text-3xl py-1 dark:text-white">What I do</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">From the very beginning of my carreer I've been working on interactive 
+            <span className="text-teal-500"> educational </span>projects.</p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">I <span className="text-teal-500"> love </span>Front-end development and game designing because I can have a visual feedback in real time as I code.</p>
+          </div>
+
+          <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
+              <Image src={design} width={100} height={100} alt="design icon"/>
+              <h3 className="text-lg font-medium pt-8 pb-2  ">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs following core
+                design theory.
+              </p>
+              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Indesign</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+              <Image src={code} width={100} height={100} alt="code icon"/>
+              <h3 className="text-lg font-medium pt-8 pb-2 ">
+                Code your dream project
+              </h3>
+              <p className="py-2">
+                Do you have an idea for your next great website? Let's make it a
+                reality.
+              </p>
+              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Indesign</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+              <Image src={consulting} width={100} height={100} alt="consulting icon"/>
+              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
+              <p className="py-2">
+                Are you interested in feedback for your current project? I can
+                give you tips and tricks to level it up.
+              </p>
+              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Indesign</p>
+            </div>
+          </div>
+        
         </section>
 
       </main>
