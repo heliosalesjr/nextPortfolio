a0e72d84102970c2ff7adf493e21941de57a589a
heliosalesjr <heliosalesjr@gmail.com>
2023-09-18 20:45:09 -0300

    added portfolio section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
+// @useClient
+import React from 'react';
 import Head from "next/head"
 import { BsFillMoonStarsFill } from 'react-icons/bs'
+import { useState } from "react";
 import {AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube} from 'react-icons/ai'
 import heliogiraffe from "../public/heliogiraffe 1.png"
 import Image from "next/image"
 import design from "../public/design.png"
 import code from "../public/code.png"
 import consulting from "../public/consulting.png"
+import web1 from "../public/web1.png"
+import web2 from "../public/web2.png"
+import web3 from "../public/web3.png"
+import web4 from "../public/web4.png"
+import web5 from "../public/web5.png"
+import web6 from "../public/web6.png"
+
+
 //https://github.com/developedbyed/react-portofolio-with-tailwind/blob/main/pages/index.js
 
 export default function Home() {
+
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <>
       <Head>
@@ -110,7 +124,79 @@ export default function Home() {
           </div>
         
         </section>
-
+        <section className="py-10">
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white font-semibold ">Portofolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Since the beginning of my journey as a freelance designer and
+              developer, I've done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1 ">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web1}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web2}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web3}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web4}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web5}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web6}
+              />
+            </div>
+          </div>
+        </section>
       </main>
       
     
